3fd995ea00cf359a22c5fdde64b60807ae813ca0
loyal <loyaltly.cn@gmail.com>
2024-06-01 15:21:35 +0800

    add theme

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,19 +1,5 @@
 <template>
    <div>
-       <div class="flex ">
-           <div>sdlfkdslkh</div><div>sdlfkdslkh</div><div>sdlfkdslkh</div><div>sdlfkdslkh</div><div>sdlfkdslkh</div>
-           <div>sdlfkdslkh</div>
-
-           <theme-select/>
-           <div>sdlfkdslkh</div>
-           <div>sdlfkdslkh</div>
-           <div>sdlfkdslkh</div>
-           <div>sdlfkdslkh</div>
-           <div>sdlfkdslkh</div>
-
-
-
-       </div>
        <router-view/>
    </div>
 </template>
